1d5e9a1d3257cb85d2213e3dcd9897cc9175458a
EMBVERSE LLP <shaunak.vyas1@gmail.com>
2024-03-29 14:36:36 +0530

    version 0.0.1

--- Arduino_Modbus_RTU_Slave/ard_mrtus/ard_mrtus.ino
+++ Arduino_Modbus_RTU_Slave/ard_mrtus/ard_mrtus.ino
@@ -39,49 +39,63 @@
 #define RESET 0
 #define SET !RESET
 
-uint16_t tcount = 0;
 uint8_t ch_count = 0;
 uint16_t timer_count = 0;
-bool one_ms_timer_flag = RESET, data_received_flag = RESET, blank_data_received_flag = RESET, timeout_flag = RESET, data_length_overflow_flag = RESET;
-uint8_t modbus_received_data[64];
-uint8_t uart_data_rx[64];
+uint8_t modbus_received_data[(NUMBER_OF_HOLDING_REGISTERS*2)+7];
+uint8_t uart_data_rx[(NUMBER_OF_HOLDING_REGISTERS*2)+7];
 typedef enum {MODBUS_IDLE, MODBUS_Rx_DATA_COLLECTION, MODBUS_Rx_DATA_RECEIVED} UART_fsm;
-UART_fsm fsm = MODBUS_IDLE;
+UART_fsm fsm = MODBUS_IDLE; // fsm - finite state machine
 void initialize_eeprom(void);
 void read_eeprom(void);
 void update_eeprom(uint8_t parameter);
 
+/*******************************************************************************************
+*Function Name : initialize_eeprom
+*Description : 
+  Initialises EEPROM by setting default values for Modbus parameters decided by developer.
+  Arguments :
+    None
+  Return : None
+  Note : 
+    This function is invoked once the fresh piece of chip with EEPROM not written, is
+    being programmed the very first time.
+*********************************************************************************************/
 void initialize_eeprom(void)
 {    
     for(uint8_t i = 0; i < COIL_BYTES; i++)
     {
         modbus_coil_value[i] = 0xFF;
         modbus_response_coil_temp[i] = modbus_coil_value[i];
-        //EEPROM.write(COIL_BEGIN_EEPROM_ADDRESS + i, modbus_coil_value[i]);
     }    
     update_eeprom(COIL);   
     for(uint8_t i = 0; i < DISCRETE_INPUT_BYTES; i++)
     {
         modbus_discrete_input_value[i] = 0xFF;                
         modbus_response_discrete_input_temp[i] = modbus_discrete_input_value[i];
-        //EEPROM.write(COIL_BEGIN_EEPROM_ADDRESS + i, modbus_discrete_input_value[i]);
     }
     update_eeprom(DISCRETE_INPUT);
     for(uint8_t i = 0; i < NUMBER_OF_HOLDING_REGISTERS; i++)
     {   
         modbus_holding_registers_value[i] = 0xFFFF;
         modbus_response_holding_registers_temp[i] = modbus_holding_registers_value[i];
-        //EEPROM.write(COIL_BEGIN_EEPROM_ADDRESS + i, modbus_holding_registers_value[i]);
     }
     update_eeprom(HOLDING_REGISTER);
     for(uint8_t i = 0; i < NUMBER_OF_INPUT_REGISTERS; i++)
     {
         modbus_input_registers_value[i] = 0xAA55;
         modbus_response_input_registers_temp[i] =  modbus_input_registers_value[i];
-        //EEPROM.write(COIL_BEGIN_EEPROM_ADDRESS + i, modbus_coil_value[i]);
     }
     update_eeprom(INPUT_REGISTER);
 }
+
+/*******************************************************************************************
+*Function Name : read_eeprom
+*Description : 
+  Reads EEPROM with the values for Modbus parameters and saves them in variables.
+  Arguments :
+    None
+  Return : None
+*********************************************************************************************/
 void read_eeprom(void)
 {
     for(uint8_t i = 0; i < COIL_BYTES; i++)
@@ -107,6 +121,15 @@ void read_eeprom(void)
         modbus_response_input_registers_temp[i] = modbus_input_registers_value[i];
     }
 }
+
+/*******************************************************************************************
+*Function Name : update_eeprom
+*Description : 
+  Updates EEPROM with the values set by Modbus Master for Modbus parameters.
+  Arguments :
+    uint8_t - Function Index of Modbus
+  Return : None
+*********************************************************************************************/
 void update_eeprom(uint8_t parameter)
 {
     switch(parameter)
@@ -145,6 +168,20 @@ void update_eeprom(uint8_t parameter)
             break;            
     }
 }
+
+/*******************************************************************************************
+*Function Name : Timer0_initialize
+*Description : 
+  Sets Timer0 to trigger every 1 ms if enabled.
+  Arguments :
+    None
+  Return : None
+  Note :
+    This timer is needed to decide whether a valid Modbus Query Data Frame is received from
+    Master. The minimum time it takes between consecutive Data Frames to receive is 4 ms. So,
+    this timer checks that interval and sets a data reception flag if the bus is IDLE for 4
+    or more ms.
+*********************************************************************************************/
 void Timer0_initialize(uint32_t time_period_us)
 {
     //set timer0 interrupt at 2kHz
@@ -159,7 +196,8 @@ void Timer0_initialize(uint32_t time_period_us)
     TCCR0B |= (1 << CS01) | (1 << CS00);   
     // enable timer compare interrupt
     TIMSK0 |= (1 << OCIE0A);//set timer0 interrupt at 2kHz
-}    
+}  
+
 /********************************************************************************************************************
   Function Name : USART_init
   Description : 
@@ -169,7 +207,7 @@ void Timer0_initialize(uint32_t time_period_us)
     - unsigned int ubrr : Baudrate Setting. Refer Technical Reference Manual for getting this value.
   Return : Nothing
   Note : 
-    The value of ubrr in this code is set to "16" for 115200 baudrate. Refer Technical Reference 
+    The value of ubrr in this code is set to "103" for 9600 baudrate. Refer Technical Reference 
     Manual of AVR for better understanding.
 ********************************************************************************************************************/
 void USART_Init(unsigned int ubrr)
@@ -186,6 +224,7 @@ void USART_Init(unsigned int ubrr)
     // Enable the USART Receive Complete interrupt (USART_RXC)
     UCSR0B |= (1 << RXCIE0); 
 }
+
 /******************************************************************************
   Function Name : USART_Transmit_Str
   Description : 
@@ -194,72 +233,64 @@ void USART_Init(unsigned int ubrr)
     - unsigned char *data : Pointer to the start address of Data array
   Return : Nothing
  ******************************************************************************/
-void Modbus_RTU_Over_UART_PutChar(char dat_a)  //
+void Modbus_RTU_Over_UART_PutChar(char ch_data)  //
 {
-    //while(c > 0)
-    //{  
-        // Wait for empty transmit buffer
         while ( !( UCSR0A & (1<<UDRE0)) );
         // Put data into buffer, sends the data
-        UDR0 = dat_a;
-        
-    //}
+        UDR0 = ch_data;
 }
-/*End of Function USART_Transmit_Str*/
+
 ISR(TIMER0_COMPA_vect)
 {   
-    tcount++;
-    if(tcount >= 4)
+    timer_count++;
+    if(timer_count >= 4)
     {
-        tcount = 0;
+        timer_count = 0;
         fsm =  MODBUS_Rx_DATA_RECEIVED;
-        TIMSK0 &= (0 << OCIE0A);
-        STATUS_LED_TOGGLE;
-    }    
-  //TIMSK0 |= (1 << OCIE0A);
+        TIMSK0 &= (0 << OCIE0A); // Disable Timer0
+        STATUS_LED_TOGGLE; // Indicative LED Toggling on Data Frame Reception 
+    }
 }
+
 void setup()
 {
-    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT); // STATUS LED that toggles on every Query Reception 
     digitalWrite(LED_BUILTIN, LOW);
-    uint8_t EEPROM_PowerUp_flag;
+    uint8_t EEPROM_PowerUp_flag; // EEPROM initialization (Once only)
     EEPROM_PowerUp_flag = EEPROM.read(FIRST_POWERUP_EEPROM_ADDRESS);
-    if(EEPROM_PowerUp_flag == 0xFF) 
+    if(EEPROM_PowerUp_flag == 0xFF) // Indicates to write EEPROM with default modbus parameter values (set by programmer) 
     {
         initialize_eeprom();
-        EEPROM_PowerUp_flag = 0x01;
+        EEPROM_PowerUp_flag = 0x01; // Next Power Cycles won't initialize EEPROM
         EEPROM.write(FIRST_POWERUP_EEPROM_ADDRESS, EEPROM_PowerUp_flag);
     }    
-    read_eeprom();  
-  /* Call Tlc.init() to setup the tlc.
-     You can optionally pass an initial PWM value (0 - 4095) for all channels.*/
-    //Serial.begin(9600);
+    read_eeprom(); // Reads the EEPROM and stores values in variables  
     USART_Init(103); // Initializing Serial UART
-    
     Timer0_initialize(1000);
-    TIMSK0 &= (0 << OCIE0A);//set timer0 interrupt at 2kHz
-    sei();
+    TIMSK0 &= (0 << OCIE0A);// Disable Timer0
+    sei(); // Enable Interrupts
 }
+
 void loop()
 {
-    if(fsm == MODBUS_Rx_DATA_RECEIVED)
+    if(fsm == MODBUS_Rx_DATA_RECEIVED) // Query from Modbus master is received
     {            
         for(uint8_t i = 0; i <= ch_count; i++)
         {
-            modbus_received_data[i] = uart_data_rx[i];
+            modbus_received_data[i] = uart_data_rx[i]; // Store the dataframe in an array
         }
-        if(modbus_received_data[0] == MODBUS_SLAVE_ADDRESS)
+        if(modbus_received_data[0] == MODBUS_SLAVE_ADDRESS) // Check Modbus Slave Address
         {
-            switch(modbus_received_data[1])
+            switch(modbus_received_data[1]) // Check the Function Index
             {
-                case 0x01:
+                case 0x01: //Read Coils
                     query_response_read_coil(&modbus_received_data[0], ch_count + 1);
                     for(uint8_t i = 0; i < modbus_response_data_length; i++)
                     {
                         Modbus_RTU_Over_UART_PutChar(modbus_response_read_coil[i]);
                     }
                     break;
-                case 0x02:
+                case 0x02: //Read Discrete Inputs
                     query_response_read_discrete_input(&modbus_received_data[0], ch_count + 1);
             
                     for(uint8_t i = 0; i < modbus_response_data_length; i++)
@@ -267,21 +298,21 @@ void loop()
                         Modbus_RTU_Over_UART_PutChar(modbus_response_read_discrete_input[i]);
                     }
                     break;
-                case 0x03 :
+                case 0x03 : // Read Holding Registers
                     query_response_read_holding_registers(&modbus_received_data[0], ch_count + 1);
                     for(uint8_t i = 0; i < modbus_response_data_length; i++)
                     {
                         Modbus_RTU_Over_UART_PutChar(modbus_response_read_holding_registers[i]);
                     }
                     break;
-                case 0x04 :
+                case 0x04 : //Read Input Registers
                     query_response_read_input_registers(&modbus_received_data[0], ch_count + 1);
                     for(uint8_t i = 0; i < modbus_response_data_length; i++)
                     {
                         Modbus_RTU_Over_UART_PutChar(modbus_response_read_input_registers[i]);
                     }
                     break;
-                case 0x05 :
+                case 0x05 : // Write Single Coil
                     query_response_force_single_coil(&modbus_received_data[0], ch_count + 1);
                     for(uint8_t i = 0; i < modbus_response_data_length; i++)
                     {
@@ -289,7 +320,7 @@ void loop()
                     }
                     update_eeprom(COIL);
                     break;
-                case 0x06 :
+                case 0x06 : // Write Single Register
                     query_response_preset_single_register(&modbus_received_data[0], ch_count + 1);
                     for(uint8_t i = 0; i < modbus_response_data_length; i++)
                     {
@@ -297,7 +328,7 @@ void loop()
                     }
                     update_eeprom(HOLDING_REGISTER);
                     break;
-                case 0x0F :
+                case 0x0F : // Write Multiple Coils
                     query_response_force_multiple_coils(&modbus_received_data[0], ch_count + 1);
                     for(uint8_t i = 0; i < modbus_response_data_length; i++)
                     {
@@ -305,7 +336,7 @@ void loop()
                     }
                     update_eeprom(COIL);
                     break;
-                case 0x10 :
+                case 0x10 : // Write Multiple Registers
                     query_response_preset_multiple_registers(&modbus_received_data[0], ch_count + 1);
                     for(uint8_t i = 0; i < modbus_response_data_length; i++)
                     {
@@ -316,18 +347,18 @@ void loop()
                 default :
                     break;                        
             }
-        }     
+        }
         fsm = MODBUS_IDLE;
         ch_count = 0;
     }
 }
-// ISR(USART0_RX_vect) //Arduino Mega
-ISR(USART_RX_vect) //Arduino Uno
+
+ISR(USART0_RX_vect) // Uncomment For Arduino Mega
+//ISR(USART_RX_vect) // Uncomment for Arduino Uno
 {  
-    
     char incoming_Char = UDR0;
-    tcount = 0;
-    TIMSK0 |= (1 << OCIE0A);
+    timer_count = 0;
+    TIMSK0 |= (1 << OCIE0A); // Enable Timer0
     switch(fsm)
     {
         case MODBUS_IDLE:
@@ -336,8 +367,8 @@ ISR(USART_RX_vect) //Arduino Uno
             if(uart_data_rx[0] == MODBUS_SLAVE_ADDRESS)
             {
                 fsm = MODBUS_Rx_DATA_COLLECTION;
-                tcount = 0;
-                TIMSK0 |= (1 << OCIE0A);
+                timer_count = 0;
+                TIMSK0 |= (1 << OCIE0A); // Enable Timer0
                 break;
             }
             else
@@ -348,10 +379,9 @@ ISR(USART_RX_vect) //Arduino Uno
         case MODBUS_Rx_DATA_COLLECTION:
         {
             ch_count++;
-            uart_data_rx[ch_count] = incoming_Char;
-            
-            tcount = 0;
-            TIMSK0 |= (1 << OCIE0A);
+            uart_data_rx[ch_count] = incoming_Char;        
+            timer_count = 0;
+            TIMSK0 |= (1 << OCIE0A); // Enable Timer0
             break;
         }
     }
